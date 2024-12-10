b1f5c1c42864e75f78529f4278eb657bc04d4e68
TDTxLE <tdtxle@gmail.com>
2024-12-10 11:14:17 -0600

    agreggar grafica sircular

---
+++ lib/utils/colors.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+const colorsCharts = [
+  Color(0xffFF5733), //  (Rojo anaranjado brillante)
+  Color(0xff33FF57), //  (Verde brillante)
+  Color(0xff3357FF), //  (Azul brillante)
+  Color(0xffFF33A1), //  (Rosa vibrante)
+  Color(0xffFFC300), //  (Amarillo dorado)
+  Color(0xffDAF7A6), //  (Verde pastel)
+  Color(0xff581845), //  (Morado oscuro)
+  Color(0xffC70039), //  (Rojo carmesí)
+  Color(0xff900C3F), //  (Rojo vino)
+  Color(0xff900CFF), //  (Violeta brillante)
+  Color(0xff33FFF6), //  (Turquesa claro)
+  Color(0xffFF5733), //  (Naranja cálido)
+  Color(0xff75A3FF), //  (Azul pastel)
+  Color(0xffA1FF33), //  (Verde lima)
+  Color(0xffF733FF), //  (Magenta eléctrico)
+  Color(0xffFFFF33), //  (Amarillo puro)
+  Color(0xffFF8C33), //  (Naranja quemado)
+  Color(0xff6C33FF), //  (Morado medio)
+  Color(0xff33FFF1), //  (Aqua suave)
+  Color(0xff33FF83), //  (Verde menta)
+];
