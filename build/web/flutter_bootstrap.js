075f1eef5e43119674ddad526c32284dc8208200
TDTxLE <tdtxle@gmail.com>
2024-12-11 09:28:43 -0600

    .

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3602109990"
+    serviceWorkerVersion: "3024119276"
   }
 });
